07369900a68b53afc6b5063f75829287bae42301
juan muñoz <juaz2560@gmail.com>
2024-02-22 07:53:56 -0500

    ya

--- modelo/conexion.php
+++ modelo/conexion.php
@@ -1,6 +1,16 @@
 <?php
-$conexion=new mysqli("localhost","root","diego041012","crud_php");
-$conexion->set_charset("utf8");
 
+$servidor="localhost";
+$user="root";
+$password="";
+$basedatos="crud_php";
 
+$conexion=new mysqli($servidor, $user, $password, $basedatos);//Ya la conexion esta lista
+
+//Comprobar si existe algun error de conexion
+if($conexion->connect_errno)//connect_errno verifica un error de conectividad
+{
+    echo "Nuestra conexion presenta fallas";
+    exit();
+}
 ?>
